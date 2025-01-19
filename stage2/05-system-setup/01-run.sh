ece712cbb8a9ee6d1573eff38ed9b632b4db06f9
Chris <chris@tgu-ltd.uk>
2025-01-19 04:50:41 +0000

    Rebranded and platform vars added

--- stage2/05-system-setup/01-run.sh
+++ stage2/05-system-setup/01-run.sh
@@ -3,21 +3,6 @@
 source ../stage-vars.sh
 
 ###
-### TGU Specific variables
-###
-### Need to put this into another/global file 
-###
-
-#DNSMASQ_IP="10.10.17.1"
-#DNSMASQ_NET="10.10.17.0/24"
-#DNSMASQ_START="10.10.17.2"
-#DNSMASQ_END="10.10.17.255"
-#HOSTAPD_PASS="gps@pi!9"
-#HOSTAPD_NAME="stratum1pi"
-
-
-
-###
 # CMDLINE.TXT
 ###
 CMDLINE_TXT="${ROOTFS_DIR}/boot/firmware/cmdline.txt"
